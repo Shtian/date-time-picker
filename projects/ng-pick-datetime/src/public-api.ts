9d703d81f0fa708ce0faab6cccc8d88bf58c20ea
Stian Haga <hagastian@gmail.com>
2020-11-20 14:45:33 +0100

    fix remove moment adapter from public api

--- projects/ng-pick-datetime/src/public-api.ts
+++ projects/ng-pick-datetime/src/public-api.ts
@@ -12,15 +12,6 @@ export {
     OWL_DATE_TIME_LOCALE_PROVIDER,
 } from './lib/date-time/adapter/date-time-adapter.class';
 export { OwlDateTimeFormats, OWL_DATE_TIME_FORMATS } from './lib/date-time/adapter/date-time-format.class';
-export {
-    MomentDateTimeAdapter,
-    OwlMomentDateTimeAdapterOptions,
-    OWL_MOMENT_DATE_TIME_ADAPTER_OPTIONS,
-} from './lib/date-time/adapter/moment-adapter/moment-date-time-adapter.class';
-export {
-    MomentDateTimeModule,
-    OwlMomentDateTimeModule,
-} from './lib/date-time/adapter/moment-adapter/moment-date-time.module';
 
 export { OwlDateTimeModule } from './lib/date-time/date-time.module';
 export { OwlNativeDateTimeModule } from './lib/date-time/adapter/native-date-time.module';
