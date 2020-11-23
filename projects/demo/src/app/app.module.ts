1171297fdf0849efaceaa8d5e09cb757a4931c60
Stian Haga <hagastian@gmail.com>
2020-11-23 10:16:34 +0100

    fix: remove moment import from demo

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { OwlDateTimeModule, OwlMomentDateTimeModule } from '@busacca/ng-pick-datetime';
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from '@busacca/ng-pick-datetime';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,7 +13,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         AppRoutingModule,
         //
         OwlDateTimeModule,
-        OwlMomentDateTimeModule,
+        OwlNativeDateTimeModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
